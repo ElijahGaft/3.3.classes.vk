f53b382c5405e04b3aa87283c42d76433af7b6b7
ElijahGaft <gaft.ilya.m@yandex.ru>
2019-07-16 18:28:43 +0300

    рабочий вариант проги

--- main.py
+++ main.py
@@ -15,13 +15,15 @@ Auth_DATA = {
 
 TOKEN = '4c4851cbaeeb90b218be4aaf9dfbdd4fb43d673e7cbf48397127f43181a02fda2ed7dce5e014e9ab818da'
 class User:
-    def __init__(self, token):
+    def __init__(self,user_id, token):
         self.token = token
+        self.user_id = user_id
 
     def get_params(self):
         return {
             'access_token' : self.token,
-            'v' : '5.25'
+            'v' : '5.25',
+            'user_id' : self.user_id
         }
 
     def request(self, method, params):
@@ -32,24 +34,32 @@ class User:
         return response
 
 
-    def get_status(self):
+    def get_friends(self):
         params = self.get_params()
         response = self.request(
-            'status.get',
-            params=params
-        )
-        return response.json()['response']['text']
-
-    def set_status(self, text):
-        params = self.get_params()
-        params['text'] = text
-        response = self.request(
-            'status.set',
+            'friends.get',
             params=params
         )
         return response.json()['response']
 
-Ilya = User(TOKEN)
-Ilya.set_status('твои мысли становятся твоей жизнью')
-status = Ilya.get_status()
-print(status)
+    def __and__(self, other_user, mutal_user_lust=list()):
+        user_1 = other_user.get_friends()
+        user_2= self.get_friends()
+        for i in user_1.get('items'):
+            # print(i)
+            for ii in user_2.get('items'):
+                if i == ii:
+                    adress = 'https://vk.com/id' + str(ii)
+                    mutal_user_lust.append(adress)
+        return mutal_user_lust
+
+    def user(self, id):
+        response = requests.get(
+            'https://vk.com/id' + id,
+        )
+        return response
+
+
+Alina = User('61125176',TOKEN)
+Ilya = User('306008470',TOKEN)
+print(Alina & Ilya)
